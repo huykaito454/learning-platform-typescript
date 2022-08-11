fbad3fa3db03ab94da59afc0927c512e8055fefb
Tien01els <tien140801@gmail.com>
2022-08-11 14:13:38 +0700

    Update

--- src/store/user/services.tsx
+++ src/store/user/services.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { API_URL } from '../../constants'
 export default function getUser(id: number) {
-  return axios.get(`${API_URL}login`, {
+  return axios.post(`${API_URL}login`, {
     params: { id: id },
   })
 }
